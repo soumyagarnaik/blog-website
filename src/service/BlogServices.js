2acca0804b6b39703a260acda539d756609d2e5f
Soumya Ranjan Garnaik <soumyagarnaik1@gmail.com>
2023-02-05 11:34:22 +0530

    remove apis

--- src/service/BlogServices.js
+++ src/service/BlogServices.js
@@ -1,22 +1,5 @@
 import axios from "axios";
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
-//   params: {
-//     q: 'taylor swift',
-//     pageNumber: '1',
-//     pageSize: '100',
-//     autoCorrect: 'true',
-//     fromPublishedDate: 'null',
-//     toPublishedDate: 'null'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': '972618794bmsh1e888b594a8849dp102d52jsna93ca173b8bd',
-//     'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-//   }
-// };
-
 class BlogServices {
     getListOfblogs() {
      return axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=5d20677c729841888f2e9ac1f6b0661a').then(response => {
